f380a0429ba85d8356369b55d661b35bc4c5a358
chantelleperera <chantelle.perera@gmail.com>
2017-05-06 11:35:06 +1000

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
   });
 
   // Use the scrollomatic plugin for smooth scrolling.
-  $('nav a, .logopos').scrollomatic({
+  $('nav a, logopos').scrollomatic({
     duration: 300
   });
   
